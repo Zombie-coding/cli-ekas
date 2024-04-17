6fad6f5de2390d9e5912897e4746c73ec7d3aee5
Zombie <115482731+Zombie-coding@users.noreply.github.com>
2024-04-17 13:07:43 +0300

    Create Client v0.5

---
+++ components/elements/AuthPage/EmailInput.tsx
@@ -0,0 +1,24 @@
+import { IAuthPageInput } from '@/types/auth'
+import styles from '@/styles/auth/index.module.scss'
+
+const EmailInput = ({ register, errors }: IAuthPageInput) => (
+  <label className={styles.form__label}>
+    <input
+      {...register('email', {
+        required: 'Sisestage E-post!',
+        pattern: {
+          value: /\S+@\S+\.\S+/,
+          message: 'Vale e-post!',
+        },
+      })}
+      className={styles.form__input}
+      type="email"
+      placeholder="Email"
+    />
+    {errors.email && (
+      <span className={styles.error_alert}>{errors.email?.message}</span>
+    )}
+  </label>
+)
+
+export default EmailInput
